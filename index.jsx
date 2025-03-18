3ce6306b18da4fb6a9346ce2ce51305224697d76
filodeca <165920102+filodeca@users.noreply.github.com>
2025-03-18 16:56:18 +0100

    commit

--- index.jsx
+++ index.jsx
@@ -6,3 +6,10 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(<App />);
 
+app.use((req, res, next) => {
+    if (req.url.endsWith('.jsx')) {
+      res.setHeader('Content-Type', 'application/javascript');
+    }
+    next();
+  });
+  
